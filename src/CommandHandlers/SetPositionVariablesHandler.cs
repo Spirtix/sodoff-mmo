7a1d18bb86d9e894410d23094afe4594c26bffb2
Spirtix <michal.prazak1@gmail.com>
2023-09-12 14:11:18 +0200

    user event position variable

--- src/CommandHandlers/SetPositionVariablesHandler.cs
+++ src/CommandHandlers/SetPositionVariablesHandler.cs
@@ -51,6 +51,9 @@ class SetPositionVariablesHandler : ICommandHandler {
         vars.Add("R", client.PlayerData.R);
         vars.Add("U", new float[] { (float)client.PlayerData.P1, (float)client.PlayerData.P2, (float)client.PlayerData.P3, (float)client.PlayerData.R1, (float)client.PlayerData.R2, (float)client.PlayerData.R3 });
         vars.Add("MID", client.ClientID);
+        string? ue = spvData.Get<NetworkObject>("p").Get<string>("UE");
+        if (ue != null)
+            vars.Add("UE", ue);
 
         container.Add(vars);
         obj.Add("arr", container);
